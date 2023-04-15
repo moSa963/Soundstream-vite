4ac1e1d5d7f13539d31c0e56152e70b995b9d46a
M S <mo.sa963@hotmail.com>
2023-04-15 23:30:38 +0300

    Add The Main Structure To App File

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { Box } from "@mui/material";
 import React from "react";
+import { Outlet } from "react-router-dom";
+import AppBar from "./components/AppBar";
+import AppMenu from "./components/AppMenu/AppMenu";
+
 
 
 const App = () => {
 
 
-
     return (
-        <p>Hello</p>
+        <Box sx={{ height: "100vh", width: "100wh", display: "flex", flexDirection: "column" }} >
+            <Box sx={{ height: "100%", width: "100%", overflow: "hidden", display: "flex" }} >
+                <Box sx={{ height: "100%", width: "100%", overflow: "auto", overflowX: "hidden" }} >
+                    <Outlet />
+                </Box>
+            </Box>
+        </Box>
     );
 }
 
