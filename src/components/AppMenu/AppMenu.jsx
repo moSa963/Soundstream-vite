6def38d0b480d62e6d2d99bc70bd668643bacba1
M S <mo.sa963@hotmail.com>
2023-04-16 21:46:39 +0300

    Add Menu Items

--- src/components/AppMenu/AppMenu.jsx
+++ src/components/AppMenu/AppMenu.jsx
@@ -1,15 +1,28 @@
 import { Divider, MenuList, Paper } from "@mui/material";
 import React from "react";
 import MenuItemLink from "./MenuItemLink";
+import { Favorite, HeartBrokenOutlined, HeatPumpRounded, Home, HomeMax, LibraryAdd, PlaylistAdd, Search } from "@mui/icons-material";
+import CreatePlaylistCard from "../CreatePlaylistCard";
 
 
-const AppMenu = () => {
+const AppMenu = ({ playlists }) => {
 
 
     return (
         <Paper elevation={0} sx={{ borderRadius: 0 }}>
+            <MenuList sx={{ height: "100%", width: 300, display: "flex", flexDirection: "column", overflow: "auto", overflowX: "hidden" }}>
+                <MenuItemLink Icon={Home} title="Home" to="home" />
+                <MenuItemLink Icon={Search} title="Search" to="search" />
+                <MenuItemLink Icon={LibraryAdd} title="Library" to="library" />
+                
+                <MenuItemLink  />
 
-            <MenuList sx={{ height: "100%", width: 400, display: "flex", flexDirection: "column", overflow: "auto" }}>
+                <CreatePlaylistCard />
+                <MenuItemLink Icon={Favorite} title="Likes" to="likes" />
+
+                <Divider sx={{ my: 2, mx: .5 }}/>
+
+                { playlists?.map((e) => <MenuItemLink key={e.id} title={e?.name} small/>) }
 
             </MenuList>
         </Paper>
