4ad44f90f10d89e735ba9f01fa26ea2605251b62
M S <mo.sa963@hotmail.com>
2023-04-18 22:48:37 +0300

    Add small size option to track component

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -3,11 +3,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 
-const Track = () => {
+const Track = ({ small }) => {
 
     return (
-        <Stack spacing={2} direction="row"  sx={{ height: "100%", minWidth: 100, p: 1, }} alignItems="center">
-            <Avatar variant="square" sx={{ width: 65, height: 65 }} />
+        <Stack spacing={2} direction="row"  sx={{ height: "100%", minWidth: 100, p: small ? 0 : 1, }} alignItems="center">
+            <Avatar variant="square" sx={{ width: small ? 35 : 65, height: small ? 35 : 65 }} />
             <Stack sx={{ "&>*:hover": { transform: "scale(1.05)" } }}>
                 <Link><Typography noWrap>{track.title}</Typography></Link>
                 <Link><Typography variant="caption" color="GrayText">{track.user.username}</Typography></Link>
