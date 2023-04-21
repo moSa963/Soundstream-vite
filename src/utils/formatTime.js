314bbd558735c7fd5fca3a31f8a7789f598a2c62
M S <mo.sa963@hotmail.com>
2023-04-21 18:28:37 +0300

    Add formatTime function that convert seconds to hh:mm:ss string

---
+++ src/utils/formatTime.js
@@ -0,0 +1,25 @@
+
+
+/**
+ * 
+ * @param {int} seconds 
+ * @returns time string hh:mm:ss
+ */
+const formatTime = (seconds) => {
+    if (seconds == null) return "0:00";
+
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds - (hours * 3600)) / 60);
+    const remainingSeconds = Math.floor(seconds - (hours * 3600) - (minutes * 60));
+  
+    var res = "";
+
+    if (hours > 0)
+    {
+        res +=  hours.toString() + ':';
+    }
+
+    return res + `${minutes.toString()}:${remainingSeconds.toString().padStart(2, 0)}`;
+}
+
+export default formatTime;
