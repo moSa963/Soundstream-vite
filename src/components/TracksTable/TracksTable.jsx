6b7459d03970546d061c40017d805cbf48336951
M S <mo.sa963@hotmail.com>
2023-04-21 21:03:29 +0300

    Remove the placeholders rows

--- src/components/TracksTable/TracksTable.jsx
+++ src/components/TracksTable/TracksTable.jsx
@@ -7,37 +7,11 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import TracksTableRow from "./TracksTableRow";
-import { LockClock, Timelapse } from "@mui/icons-material";
+import { Timelapse } from "@mui/icons-material";
 
 
-const rows = [
-    {
-        'id': 1,
-        'user': {
-            "username": "user",
-        },
-        'title': "Track name",
-        'duration': 53,
-        'explicit': false,
-        'written_by': "somebody",
-        'performed_by': "somebody",
-        'created_at': '25/52/5252'
-    },
-    {
-        'id': 2,
-        'user': {
-            "username": "user",
-        },
-        'title': "Track name",
-        'duration': 53,
-        'explicit': false,
-        'written_by': "somebody",
-        'performed_by': "somebody",
-        'created_at': '25/52/5252'
-    }
-];
 
-const TracksTable = () => {
+const TracksTable = ({ tracks }) => {
 
     return (
         <TableContainer component={Paper}>
@@ -53,7 +27,7 @@ const TracksTable = () => {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {rows.map((row, index) => <TracksTableRow row={row} key={index} index={index}/>)}
+                    {tracks?.map((track, index) => <TracksTableRow row={track} key={track?.id} index={index}/>)}
                 </TableBody>
             </Table>
         </TableContainer>
