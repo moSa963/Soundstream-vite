3e4598e1bf346fc59b5a0fd0cb4352892e5ff477
M S <mo.sa963@hotmail.com>
2023-04-21 21:12:11 +0300

    Add banner and Tracks table

--- src/pages/LikesPage.jsx
+++ src/pages/LikesPage.jsx
@@ -1,15 +1,16 @@
 import { Box } from "@mui/material";
 import React from "react";
-
+import PlaylistBanner from "../components/PlaylistBanner";
+import TracksTable from "../components/TracksTable/TracksTable";
 
 
 const LikesPage = () => {
     
 
     return (
-        <Box>
-
-
+        <Box sx={{ width: "100%" }}>
+            <PlaylistBanner type="playlist" title="Liked Songs"/>
+            <TracksTable />
         </Box>
     );
 }
