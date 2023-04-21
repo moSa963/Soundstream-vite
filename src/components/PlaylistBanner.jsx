ba67ac66dd4acc1e4e2d6b0b283412b0eaeda45e
M S <mo.sa963@hotmail.com>
2023-04-21 21:11:32 +0300

    Remove the placeholders data and Update the style

--- src/components/PlaylistBanner.jsx
+++ src/components/PlaylistBanner.jsx
@@ -3,30 +3,21 @@ import React from "react";
 
 
 
-const PlaylistBanner = () => {
+const PlaylistBanner = ({ description, title, type, avatar }) => {
 
 
     return (
-        <Paper sx={{ width: "100%", p: 5, height: 300, display: "flex", alignItems: "end",
+        <Paper sx={{ width: "100%", p: 5, minHeight: 300, display: "flex", alignItems: "center",
          background: t => `linear-gradient(to top, ${t.palette.background.paper}, ${t.palette.divider} 300px)`  }} elevation={3}>
-            <Avatar sx={{width: 220, height: 220, boxShadow: t => t.shadows[4] }} variant="square"/>
+            <Avatar src={avatar} sx={{width: 220, height: 220, boxShadow: t => t.shadows[4] }} variant="square"/>
             <Stack spacing={2} sx={{ pl: 5 }}>
-                <Typography variant="button">{playlit.album ? "album" : "playlist"}</Typography>
-                <Typography variant="h1">{playlit.title}</Typography>
-                <Typography >{playlit.description}</Typography>
+                <Typography variant="button">{type}</Typography>
+                <Typography variant="h2">{title}</Typography>
+                <Typography >{description}</Typography>
             </Stack>
         </Paper>
     );
 }
 
-const playlit = {
-    'id': 25,
-    'user': {
-        "username": "username",
-    },
-    'title': "playlist title",
-    'description': "this playlist description should be here",
-    'album': false,
-}
 
 export default PlaylistBanner;
