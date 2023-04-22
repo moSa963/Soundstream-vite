8ef529f74c1be7b38e4aa9e78d7f32d3f47feb8d
M S <mo.sa963@hotmail.com>
2023-04-22 18:52:49 +0300

    Add avatar to the app bar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import MuiAppBar from "@mui/material/AppBar";
-import { IconButton, Toolbar, alpha } from "@mui/material";
-import { List } from "@mui/icons-material";
-
+import { Avatar, Box, Toolbar } from "@mui/material";
 
 
 const AppBar = () => {
@@ -12,7 +10,10 @@ const AppBar = () => {
     return (
         <MuiAppBar elevation={0} position="sticky" color="transparent" sx={{ backdropFilter: "blur(10px)", mb: "-15px" }}>
             <Toolbar variant="dense">
-                
+
+                <Box sx={{ flexGrow: 1 }}/>
+
+                <Avatar sx={{ transform: "scale(0.95)", ":hover": { transform: "scale(1)" }, cursor: "pointer" }} />
             </Toolbar>
         </MuiAppBar>
     );
