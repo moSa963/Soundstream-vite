e66f3fb18b41fd11ca24a4dcae71d6e98c9983ae
M S <mo.sa963@hotmail.com>
2023-04-25 23:01:57 +0300

    Add api call to load a playlist's tracks

--- src/pages/ShowPlaylistPage.jsx
+++ src/pages/ShowPlaylistPage.jsx
@@ -2,19 +2,33 @@ import { Box } from "@mui/material";
 import React from "react";
 import TracksTable from "../components/TracksTable/TracksTable";
 import PlaylistBanner from "../components/PlaylistBanner";
+import request from "../utils/Request";
+import { useLoaderData } from "react-router-dom";
 
 
 
 const ShowPlaylistPage = () => {
+    const { data } = useLoaderData();
+    const [tracks, setTracks] = React.useState([]);
     
+    React.useEffect(() => {
+        loadTracks(data, setTracks);
+    }, []);
 
     return (
         <Box sx={{ width: "100%" }}>
-            <PlaylistBanner type="playlist" title="playlist name"/>
-            <TracksTable />
+            <PlaylistBanner title={data.title} description={data.description} type={"playlist"}/>
+            <TracksTable tracks={tracks} setTracks={setTracks}/>
         </Box>
     );
 }
 
+const loadTracks = async (playlist, setData) => {
+    const res = await request(`api/playlists/${playlist.id}/tracks`);
 
+    if (res.ok){
+        const js = await res.json();
+        setData(js.data);
+    }
+}
 export default ShowPlaylistPage;
