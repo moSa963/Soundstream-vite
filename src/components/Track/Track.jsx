feaf70dc5efe9d1136d03b020865193ee5d876f4
M S <mo.sa963@hotmail.com>
2023-04-25 22:54:33 +0300

    Remove the placeholder object

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -3,10 +3,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 
-const Track = ({ small, children }) => {
+const Track = ({ small, children, track }) => {
     
     return (
-        <Stack spacing={2} direction="row"  sx={{ height: "100%", minWidth: 100, p: small ? 0 : 1, }} alignItems="center">
+        <Stack spacing={2} direction="row"  sx={{ width: "100%", height: "100%", minWidth: 100, p: small ? 0 : 1, }} alignItems="center">
             <Avatar variant="square" sx={{ width: small ? 35 : 65, height: small ? 35 : 65 }} />
             <Stack sx={{ "&>*:hover": { transform: "scale(1.05)" } }}>
                 <Link><Typography noWrap>{track.title}</Typography></Link>
@@ -16,18 +16,5 @@ const Track = ({ small, children }) => {
         </Stack>
     );
 }
-
-const track = {
-    'id': 250,
-    'user': {
-        "username": "user",
-    },
-    'title': "Track name",
-    'duration': 53,
-    'explicit': false,
-    'written_by': "somebody",
-    'performed_by': "somebody",
-}
-
 
 export default Track;
