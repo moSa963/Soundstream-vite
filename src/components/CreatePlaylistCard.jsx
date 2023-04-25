41d6741c7e4950b39aaad941269969d71eb433d1
M S <mo.sa963@hotmail.com>
2023-04-25 22:49:35 +0300

    Add api call to create new playlist

--- src/components/CreatePlaylistCard.jsx
+++ src/components/CreatePlaylistCard.jsx
@@ -2,16 +2,18 @@ import { Backdrop, Button, Paper, Stack, TextField } from "@mui/material";
 import React from "react";
 import MenuItemLink from "./AppMenu/MenuItemLink";
 import { PlaylistAdd } from "@mui/icons-material";
+import request from "../utils/Request";
 
 
 
-const CreatePlaylistCard = () => {
+const CreatePlaylistCard = ({ onPlaylistAdded }) => {
     const [open, setOpen] = React.useState(false);
+    const [title, setTitle] = React.useState("");
 
     const handleAdd = () => {
-
+        title && create(title, onPlaylistAdded);
+        setOpen(false);
     }
-
 
     return (
         <React.Fragment>
@@ -20,7 +22,7 @@ const CreatePlaylistCard = () => {
             <Backdrop open={open} onClick={(e) => e.currentTarget == e.target && setOpen(false)} sx={{ zIndex: 10000 }}>
                 <Paper sx={{ width: "100%", p: 3, maxWidth: 550, }}>
                     <Stack spacing={2} sx={{ width: "100%", alignItems: "end" }}>
-                        <TextField fullWidth placeholder="Title..." />
+                        <TextField fullWidth placeholder="Title..." value={title} onChange={(e) => setTitle(e.target.value)}/>
                         <Stack direction="row" spacing={2}>
                             <Button onClick={handleAdd}>Add</Button>
                             <Button color="error" onClick={(e) => setOpen(false)}>Cancel</Button>
@@ -32,5 +34,14 @@ const CreatePlaylistCard = () => {
     );
 }
 
+const create = async (title, onPlaylistAdded) => {
+    const res = await request("api/playlists", "POST", { title });
+
+    if (res.ok)
+    {
+        const js = await res.json();
+        onPlaylistAdded(js.data);
+    }
+}
 
 export default CreatePlaylistCard;
