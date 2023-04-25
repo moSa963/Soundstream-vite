1cf01c207ffcc949985ee0edb88f2afd811258a5
M S <mo.sa963@hotmail.com>
2023-04-25 22:53:19 +0300

    Add api call to like or unlike a track

--- src/components/Track/LikeTrackButton.jsx
+++ src/components/Track/LikeTrackButton.jsx
@@ -1,21 +1,27 @@
 import { Favorite, FavoriteBorder, FavoriteOutlined } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
 import React from "react";
+import request from "../../utils/Request";
 
 
 
-const LikeTrackButton = ({ track }) => {
+const LikeTrackButton = ({ track, setLiked }) => {
 
 
     return (
-        <IconButton title="like" onClick={() => like(track)}>
+        <IconButton title="like" onClick={() => like(track, setLiked)}>
             {track?.liked ? <Favorite /> : <FavoriteBorder /> }
         </IconButton>
     );
 }
 
-const like = async (track) => {
-    
+const like = async (track, setLiked) => {
+    const res = await request(`api/likes/tracks/${track?.id}`, track?.liked ? "DELETE" : "POST");
+
+    if (res.ok)
+    {
+        setLiked(!track?.liked);
+    }
 }
 
 export default LikeTrackButton;
