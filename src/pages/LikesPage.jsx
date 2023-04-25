d72aac969826c8f82292e516a9ef4217fe63d945
M S <mo.sa963@hotmail.com>
2023-04-25 23:01:01 +0300

    Add api call to load liked tracks

--- src/pages/LikesPage.jsx
+++ src/pages/LikesPage.jsx
@@ -2,18 +2,31 @@ import { Box } from "@mui/material";
 import React from "react";
 import PlaylistBanner from "../components/PlaylistBanner";
 import TracksTable from "../components/TracksTable/TracksTable";
+import request from "../utils/Request";
 
 
 const LikesPage = () => {
-    
+    const [tracks, setTracks] = React.useState([]);
+
+    React.useEffect(() => {
+        loadData(setTracks);
+    }, []);
 
     return (
-        <Box sx={{ width: "100%" }}>
-            <PlaylistBanner type="playlist" title="Liked Songs"/>
-            <TracksTable />
+        <Box sx={{ width: "100%" }} >
+            <PlaylistBanner type="playlist" title="Liked Songs" />
+            <TracksTable tracks={tracks} setTracks={setTracks}/>
         </Box>
     );
 }
 
+const loadData = async (setData) => {
+    const res = await request("api/likes");
+
+    if (res.ok){
+        const js = await res.json();
+        setData(js.data);
+    }
+}
 
 export default LikesPage;
