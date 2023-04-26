f22b38dc5b9046098fc271e8cc38f443c80d8f5c
M S <mo.sa963@hotmail.com>
2023-04-27 00:15:47 +0300

    Update CardsSection to map a list of data

--- src/components/CardsSection/CardsSection.jsx
+++ src/components/CardsSection/CardsSection.jsx
@@ -1,22 +1,16 @@
-import { Box, Button, Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
-import Card from "./Card";
 
 
-
-const CardsSection = ({ title, onShowAll }) => {
-
+const CardsSection = ({ title, onShowAll, data, Card }) => {
 
 
     return (
         <Stack spacing={2} sx={{ width: "100%", p: 2 }}>
             <Typography variant="h4" fontFamily="cursive">{title}</Typography>
 
-            <Stack direction="row" >
-                <Card src="vite.svg" title="Title" description="description " />
-                <Card src="vite.svg" title="Title" description="description " />
-                <Card src="vite.svg" title="Title" description="description " />
-                <Card src="vite.svg" title="Title" description="description " />
+            <Stack flexWrap="wrap" direction="row" justifyContent="center">
+                {data?.map(v => <Card key={v.id} data={v} />) }
             </Stack>
 
             {onShowAll &&
