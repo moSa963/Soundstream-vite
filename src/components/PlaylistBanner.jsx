22ff596f349e5e4f21c5e9902e827ad116200cdc
M S <mo.sa963@hotmail.com>
2023-04-27 00:17:25 +0300

    Add AvatarInput instead of Avatar

--- src/components/PlaylistBanner.jsx
+++ src/components/PlaylistBanner.jsx
@@ -1,15 +1,21 @@
-import { Avatar, Paper, Stack, Typography } from "@mui/material";
+import { AddAPhoto } from "@mui/icons-material";
+import { Avatar, Box, IconButton, Paper, Stack, Typography } from "@mui/material";
 import React from "react";
+import AvatarInput from "./AvatarInput";
 
 
 
-const PlaylistBanner = ({ description, title, type, avatar }) => {
+const PlaylistBanner = ({ description, title, type, avatar, onChange }) => {
+
+
 
 
     return (
-        <Paper sx={{ width: "100%", p: 5, minHeight: 300, display: "flex", alignItems: "center",
-         background: t => `linear-gradient(to top, ${t.palette.background.paper}, ${t.palette.divider} 300px)`  }} elevation={3}>
-            <Avatar src={avatar} sx={{width: 220, height: 220, boxShadow: t => t.shadows[4] }} variant="square"/>
+        <Paper sx={{
+            width: "100%", p: 5, minHeight: 300, display: "flex", alignItems: "center",
+            background: t => `linear-gradient(to top, ${t.palette.background.paper}, ${t.palette.divider} 300px)`
+        }} elevation={3}>
+            <AvatarInput src={avatar} sx={{ width: 220, height: 220, boxShadow: t => t.shadows[4] }} onChange={onChange}/>
             <Stack spacing={2} sx={{ pl: 5 }}>
                 <Typography variant="button">{type}</Typography>
                 <Typography variant="h2">{title}</Typography>
