b8db27e1fda5e110c2ffe36ddba016ad89227c0d
M S <mo.sa963@hotmail.com>
2023-04-27 00:16:14 +0300

    Add loader data

--- src/pages/PlaylistsPage.jsx
+++ src/pages/PlaylistsPage.jsx
@@ -1,15 +1,18 @@
 import { Box } from "@mui/material";
 import React from "react";
+import CardsSection from "../components/CardsSection/CardsSection";
+import PlaylistCard from "../components/CardsSection/PlaylistCard";
+import { useLoaderData } from "react-router-dom";
 
 
 const PlaylistsPage = () => {
-    
+    const { data } = useLoaderData();
 
     return (
         <Box sx={{ width: "100%" }}>
+            <CardsSection title="Your Playlists" Card={PlaylistCard} data={data} />
         </Box>
     );
 }
-
 
 export default PlaylistsPage;
