be78e4f23fa0e728975b4c3bcfb25918b9c7ebf3
M S <mo.sa963@hotmail.com>
2023-04-26 23:18:59 +0300

    Add src to avatar

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -1,13 +1,14 @@
 import { Avatar, Stack, Typography } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
+import { APP_URL } from "../../utils/Request";
 
 
 const Track = ({ small, children, track }) => {
     
     return (
         <Stack spacing={2} direction="row"  sx={{ width: "100%", height: "100%", minWidth: 100, p: small ? 0 : 1, }} alignItems="center">
-            <Avatar variant="square" sx={{ width: small ? 35 : 65, height: small ? 35 : 65 }} />
+            <Avatar variant="square" sx={{ width: small ? 35 : 65, height: small ? 35 : 65 }} src={`${APP_URL}api/tracks/${track.id}/photo`}/>
             <Stack sx={{ "&>*:hover": { transform: "scale(1.05)" } }}>
                 <Link><Typography noWrap>{track.title}</Typography></Link>
                 <Link><Typography variant="caption" color="GrayText">{track.user.username}</Typography></Link>
