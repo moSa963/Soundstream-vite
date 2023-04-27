2a6e1c28afd9e6d1aad2e2ffed2befb9a450fe5e
M S <mo.sa963@hotmail.com>
2023-04-28 00:24:08 +0300

    Add nav function

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,17 @@
 import { Box } from "@mui/material";
 import React from "react";
 import CardsSection from "../components/CardsSection/CardsSection";
+import { useNavigate } from "react-router-dom";
 
 
 
 const HomePage = () => {
-
+    const nav = useNavigate();
 
     return (
         <Box sx={{ width: "100%" }}>
             
-            <CardsSection title="Your Playlists" onShowAll={() => console.log("all")}/>
+            <CardsSection title="Your Playlists" onShowAll={() => nav("/playlist")}/>
             <CardsSection title="Recently Played"/>
             <CardsSection title="Liked Tracks"/>
             <CardsSection title="Recent searches"/>
