525af6946c8779c160792b38f3c55406980a2321
M S <mo.sa963@hotmail.com>
2023-04-28 00:24:39 +0300

    Add Link to Card

--- src/components/CardsSection/Card.jsx
+++ src/components/CardsSection/Card.jsx
@@ -1,5 +1,6 @@
 import { Box, Paper, Typography, styled } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Image = styled('img')(() => ({
     width: "100%",
@@ -8,26 +9,29 @@ const Image = styled('img')(() => ({
     objectFit: "cover",
 }));
 
-const Card = ({ src, onClick, children }) => {
+const Card = ({ src, onClick, children, to }) => {
 
     return (
-        <Paper elevation={3} onClick={onClick}
-            sx={{
-                width: "100%",
-                maxWidth: 230,
-                minWidth: 150,
-                p: 1, 
-                overflow: "hidden",
-                cursor: "pointer",
-                m: .5,
-                boxShadow: t => t.shadows[2],
-                ":hover": { boxShadow: t => t.shadows[6], filter: "contrast(90%)" }
-            }}>
-            <Box sx={{ width: "100%", aspectRatio: '1', my: 1, boxShadow: t => t.shadows[1] }}>
-                <Image src={src} />
-            </Box>
-            {children}
-        </Paper>
+        <Link to={to}>
+            <Paper elevation={3} onClick={onClick}
+                sx={{
+                    width: "100%",
+                    maxWidth: 230,
+                    minWidth: 150,
+                    height: 350,
+                    p: 1,
+                    overflow: "hidden",
+                    cursor: "pointer",
+                    m: .5,
+                    boxShadow: t => t.shadows[2],
+                    ":hover": { boxShadow: t => t.shadows[6], filter: "contrast(90%)" }
+                }}>
+                <Box sx={{ width: "100%", aspectRatio: '1', my: 1, boxShadow: t => t.shadows[1] }}>
+                    <Image src={src} />
+                </Box>
+                {children}
+            </Paper>
+        </Link>
     );
 }
 
