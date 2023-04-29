948e1fb67b0ce4229abc162a573ad30a47d14343
M S <mo.sa963@hotmail.com>
2023-04-30 00:35:47 +0300

    Add playlists context

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import AppBar from "./components/AppBar";
 import AppMenu from "./components/AppMenu/AppMenu";
 import PlayerProvider from "./contexts/PlayerContext";
 import AuthProvider from "./contexts/AuthContext";
+import PlaylistsProvider from "./contexts/PlaylistsContext";
 
 
 
@@ -14,15 +15,17 @@ const App = () => {
     return (
         <Box sx={{ height: "100vh", width: "100wh", display: "flex", flexDirection: "column" }} >
             <AuthProvider>
-                <PlayerProvider>
-                    <Box sx={{ height: "100%", width: "100%", overflow: "hidden", display: "flex" }} >
-                        <AppMenu />
-                        <Paper sx={{ height: "100%", width: "100%", position: "relative", overflow: "auto", overflowX: "hidden", borderRadius: 0 }} >
-                            <AppBar />
-                            <Outlet />
-                        </Paper>
-                    </Box>
-                </PlayerProvider>
+                <PlaylistsProvider>
+                    <PlayerProvider>
+                        <Box sx={{ height: "100%", width: "100%", overflow: "hidden", display: "flex" }} >
+                            <AppMenu />
+                            <Paper sx={{ height: "100%", width: "100%", position: "relative", overflow: "auto", overflowX: "hidden", borderRadius: 0 }} >
+                                <AppBar />
+                                <Outlet />
+                            </Paper>
+                        </Box>
+                    </PlayerProvider>
+                </PlaylistsProvider>
             </AuthProvider>
         </Box>
     );
