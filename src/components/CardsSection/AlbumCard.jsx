0d588db50ee0ad5eb4d47dde97f4b879f75cbb20
M S <mo.sa963@hotmail.com>
2023-05-01 22:37:29 +0300

    Add album card

---
+++ src/components/CardsSection/AlbumCard.jsx
@@ -0,0 +1,18 @@
+import { Typography } from "@mui/material";
+import React from "react";
+import Card from "./Card";
+import { APP_URL } from "../../utils/Request";
+
+
+const AlbumCard = ({ data }) => {
+
+    return (
+        <Card src={`${APP_URL}api/playlists/${data.id}/photo`} to={`/library/${data.id}`}>
+            <Typography noWrap >{data?.title}</Typography>
+            <Typography variant="caption" color="gray" fontFamily="serif">{data?.description}</Typography>
+        </Card>
+    );
+}
+
+
+export default AlbumCard;
