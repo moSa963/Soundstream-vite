f0572e5c0c8ef7fd0a1cc33c93ddd0d11f640cff
M S <mo.sa963@hotmail.com>
2023-05-01 22:30:48 +0300

    Add card component to create an album

--- src/components/Cards/CreateAlbumCard.jsx
+++ src/components/Cards/CreateAlbumCard.jsx
@@ -1,26 +1,28 @@
 import { Backdrop, Button, Paper, Stack, TextField } from "@mui/material";
 import React from "react";
+import request from "../../utils/Request";
 
 
 
-const CreateAlbumCard = () => {
+const CreateAlbumCard = ({ onAlbumAdded }) => {
     const [open, setOpen] = React.useState(false);
-
-    const handleCreate = () => {
-
-    }
+    const [title, setTitle] = React.useState("");
 
     return (
         <React.Fragment>
             <Button fullWidth onClick={() => setOpen(true)} variant="text">Create New Album</Button>
-            
+
             <Backdrop open={open} onClick={(e) => e.currentTarget == e.target && setOpen(false)} sx={{ zIndex: 10000 }}>
                 <Paper sx={{ width: "100%", p: 3, maxWidth: 550, }}>
                     <Stack spacing={2} sx={{ width: "100%", alignItems: "end" }}>
-                        <TextField fullWidth placeholder="Name..." />
+                        <TextField fullWidth placeholder="Name..." value={title} onChange={(e) => setTitle(e.target.value)} />
 
                         <Stack direction="row" spacing={2}>
-                            <Button onClick={handleCreate}>Create</Button>
+                            <Button onClick={() => {
+                                create(title, onAlbumAdded)
+                                setOpen(false);
+
+                            }}>Create</Button>
                             <Button color="error" onClick={(e) => setOpen(false)}>Cancel</Button>
                         </Stack>
                     </Stack>
@@ -30,5 +32,12 @@ const CreateAlbumCard = () => {
     );
 }
 
+const create = async (title, onAlbumAdded) => {
+    const res = await request("api/albums", "POST", { title });
 
+    if (res.ok) {
+        const js = await res.json();
+        onAlbumAdded(js.data);
+    }
+}
 export default CreateAlbumCard;
