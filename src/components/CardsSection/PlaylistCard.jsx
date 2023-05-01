66e35d6b30d17cbae172cb578962bc32cae2b463
M S <mo.sa963@hotmail.com>
2023-05-01 22:39:02 +0300

    Remove unused nav

--- src/components/CardsSection/PlaylistCard.jsx
+++ src/components/CardsSection/PlaylistCard.jsx
@@ -2,11 +2,9 @@ import { Typography } from "@mui/material";
 import React from "react";
 import Card from "./Card";
 import { APP_URL } from "../../utils/Request";
-import { useNavigate } from "react-router-dom";
 
 
 const PlaylistCard = ({ data }) => {
-    const nav = useNavigate();
 
     return (
         <Card src={`${APP_URL}api/playlists/${data.id}/photo`} to={`/playlist/${data.id}`}>
