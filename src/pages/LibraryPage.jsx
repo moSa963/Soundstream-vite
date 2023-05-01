c57494cf2ce30bb1e765d20638070017a7a862d6
M S <mo.sa963@hotmail.com>
2023-05-01 22:39:47 +0300

    Add load albums tracks function

--- src/pages/LibraryPage.jsx
+++ src/pages/LibraryPage.jsx
@@ -2,18 +2,33 @@ import { Box } from "@mui/material";
 import React from "react";
 import CardsSection from "../components/CardsSection/CardsSection";
 import CreateAlbumCard from "../components/Cards/CreateAlbumCard";
+import request from "../utils/Request";
+import AlbumCard from "../components/CardsSection/AlbumCard";
 
 
 const LibraryPage = () => {
-    
+    const [albums, setAlbums] = React.useState([]);
+
+    React.useEffect(() => {
+        loadData(setAlbums);
+    }, []);
 
     return (
         <Box sx={{ width: "100%", pt: 3 }}>
-            <CreateAlbumCard />
-            <CardsSection title="Your albums"/>
+            <CreateAlbumCard onAlbumAdded={(v) => setAlbums(as => [...as, v])}/>
+            <CardsSection title="Your albums" Card={AlbumCard} data={albums} />
         </Box>
     );
 }
 
+const loadData = async (setAlbums) => {
+    const res = await request("api/albums");
+
+    if (res.ok)
+    {
+        const js = await res.json();
+        setAlbums(js.data);
+    }
+}
 
 export default LibraryPage;
