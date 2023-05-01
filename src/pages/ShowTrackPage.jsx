5f27036ae8f139aed72001f5013cab4e0f5724b0
M S <mo.sa963@hotmail.com>
2023-05-01 22:31:28 +0300

    Add showTrackPage placeholder

---
+++ src/pages/ShowTrackPage.jsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+
+
+const ShowTrackPage = () => {
+    
+
+    return (
+        <Box sx={{ width: "100%" }}>
+        </Box>
+    );
+}
+
+
+export default ShowTrackPage;
