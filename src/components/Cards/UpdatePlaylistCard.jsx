91d35fb364fe8cb8da5a7cfed138c0976bfba24b
M S <mo.sa963@hotmail.com>
2023-05-05 11:36:21 +0300

    Remove Edit button to make it external

--- src/components/Cards/UpdatePlaylistCard.jsx
+++ src/components/Cards/UpdatePlaylistCard.jsx
@@ -1,6 +1,5 @@
-import { Backdrop, Button, Divider, IconButton, Paper, Stack, TextField } from "@mui/material";
+import { Backdrop, Button, Divider, Paper, Stack, TextField } from "@mui/material";
 import React from "react";
-import { Edit } from "@mui/icons-material";
 import request from "../../utils/Request";
 import DeleteConfirmationCard from "./DeleteConfirmationCard";
 import { usePlaylists } from "../../contexts/PlaylistsContext";
@@ -8,14 +7,17 @@ import { useNavigate } from "react-router-dom";
 
 
 
-const UpdatePlaylistCard = ({ playlistId, title, description, onChange }) => {
-    const [open, setOpen] = React.useState(false);
+const UpdatePlaylistCard = ({ playlistId, title, description, onChange, open, setOpen }) => {
     const [deleteCardOpen, setDeleteCardOpen] = React.useState(false);
-    const [newTitle, setNewTitle] = React.useState(title);
-    const [newDescription, setNewDescription] = React.useState(description);
+    const [newTitle, setNewTitle] = React.useState("");
+    const [newDescription, setNewDescription] = React.useState("");
     const { setPlaylists } = usePlaylists();
     const nav = useNavigate();
 
+    React.useEffect(() => {
+        setNewTitle(title);
+        setNewDescription(description);
+    }, [title, description]);
 
     const handleSave = () => {
         save(playlistId, newTitle, newDescription, onChange);
@@ -23,36 +25,30 @@ const UpdatePlaylistCard = ({ playlistId, title, description, onChange }) => {
     }
 
     return (
-        <React.Fragment>
-            <IconButton onClick={() => setOpen(true)}>
-                <Edit />
-            </IconButton>
+        <Backdrop open={open} onClick={(e) => e.currentTarget == e.target && setOpen(false)} sx={{ zIndex: 10000 }}>
+            <Paper sx={{ width: "100%", p: 3, maxWidth: 550, height: "100%", maxHeight: 300, overflow: "auto" }}>
+                <Stack spacing={2} sx={{ width: "100%", alignItems: "end" }}>
+                    <TextField fullWidth placeholder="Title..." value={newTitle} onChange={(e) => setNewTitle(e.target.value)} label="Title" />
 
-            <Backdrop open={open} onClick={(e) => e.currentTarget == e.target && setOpen(false)} sx={{ zIndex: 10000 }}>
-                <Paper sx={{ width: "100%", p: 3, maxWidth: 550, height: "100%", maxHeight: 300, overflow: "auto" }}>
-                    <Stack spacing={2} sx={{ width: "100%", alignItems: "end" }}>
-                        <TextField fullWidth placeholder="Title..." value={newTitle} onChange={(e) => setNewTitle(e.target.value)} label="Title" />
+                    <TextField fullWidth placeholder="Description..." value={newDescription} onChange={(e) => setNewDescription(e.target.value)} label="Description" />
 
-                        <TextField fullWidth placeholder="Description..." value={newDescription} onChange={(e) => setNewDescription(e.target.value)} label="Description" />
-
-                        <Stack direction="row" spacing={2}>
-                            <Button onClick={handleSave}>Save</Button>
-                            <Button color="error" onClick={(e) => setOpen(false)}>Cancel</Button>
-                        </Stack>
-
-                        <Divider flexItem />
-
-                        <Button color="error" fullWidth onClick={() => setDeleteCardOpen(true)}>Delete</Button>
-
-                        <DeleteConfirmationCard
-                            onClose={() => setDeleteCardOpen(false)}
-                            onConfirmed={() => remove(playlistId, setPlaylists, nav)}
-                            open={deleteCardOpen}
-                            title="playlist" />
+                    <Stack direction="row" spacing={2}>
+                        <Button onClick={handleSave}>Save</Button>
+                        <Button color="error" onClick={(e) => setOpen(false)}>Cancel</Button>
                     </Stack>
-                </Paper>
-            </Backdrop>
-        </React.Fragment>
+
+                    <Divider flexItem />
+
+                    <Button color="error" fullWidth onClick={() => setDeleteCardOpen(true)}>Delete</Button>
+
+                    <DeleteConfirmationCard
+                        onClose={() => setDeleteCardOpen(false)}
+                        onConfirmed={() => remove(playlistId, setPlaylists, nav)}
+                        open={deleteCardOpen}
+                        title="playlist" />
+                </Stack>
+            </Paper>
+        </Backdrop>
     );
 }
 
