7982d7dc6d1806d5f3671f4b2b9c363547c5afd8
M S <mo.sa963@hotmail.com>
2023-05-05 22:08:30 +0300

    Fix margin

--- src/components/Cards/CardBase.jsx
+++ src/components/Cards/CardBase.jsx
@@ -6,7 +6,7 @@ import React from "react";
 const CardBase = ({ open, setOpen, children, maxWidth=550, maxHeight=300 }) => {
 
     return (
-        <Backdrop open={open} onClick={(e) => e.currentTarget == e.target && setOpen(false)} sx={{ zIndex: 10000 }}>
+        <Backdrop open={open} onClick={(e) => e.currentTarget == e.target && setOpen(false)} sx={{ zIndex: 10000, margin: "0px !important" }}>
             <Paper sx={{ width: "100%", p: 2, maxWidth: maxWidth, maxHeight: maxHeight, overflow: "auto" }}>
                 <Stack spacing={2} sx={{ width: "100%", alignItems: "end" }}>
                     {children}
