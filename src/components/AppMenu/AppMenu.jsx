5023d6b9075edfdaed2d1e126322fa4b52a880c2
M S <mo.sa963@hotmail.com>
2023-05-05 10:20:01 +0300

    Update width to be responsive and Add a link to playlist page

--- src/components/AppMenu/AppMenu.jsx
+++ src/components/AppMenu/AppMenu.jsx
@@ -1,31 +1,37 @@
-import { Divider, MenuList, Paper } from "@mui/material";
+import { Box, Divider, MenuList, Paper, Typography } from "@mui/material";
 import React from "react";
 import MenuItemLink from "./MenuItemLink";
-import { Favorite, Home, LibraryAdd, Search } from "@mui/icons-material";
+import { Add, AllInbox, Favorite, Home, LibraryAdd, PlaylistAdd, PlaylistPlay, PlusOne, Search, ShowChart } from "@mui/icons-material";
 import CreatePlaylistCard from "../Cards/CreatePlaylistCard";
 import { usePlaylists } from "../../contexts/PlaylistsContext";
+import { Link } from "react-router-dom";
 
 
 const AppMenu = () => {
-    const {playlists, setPlaylists} = usePlaylists();
+    const { playlists, setPlaylists } = usePlaylists();
 
     return (
         <Paper elevation={0} sx={{ borderRadius: 0 }}>
-            <MenuList sx={{ height: "100%", width: 300, display: "flex", flexDirection: "column", overflow: "auto", overflowX: "hidden" }}>
+            <MenuList sx={{ height: "100%", width: { xs: 55, md: 300 }, display: "flex", flexDirection: "column", overflow: "auto", overflowX: "hidden" }}>
                 <MenuItemLink Icon={Home} title="Home" to="/" />
                 <MenuItemLink Icon={Search} title="Search" to="search" />
                 <MenuItemLink Icon={LibraryAdd} title="Library" to="library" />
-                
-                <MenuItemLink  />
 
-                <CreatePlaylistCard onPlaylistAdded={(playlist) => setPlaylists(ps => [...ps, playlist])}/>
+                <MenuItemLink />
+
+                <CreatePlaylistCard onPlaylistAdded={(playlist) => setPlaylists(ps => [...ps, playlist])} />
 
                 <MenuItemLink Icon={Favorite} title="Likes" to="likes" />
 
-                <Divider sx={{ my: 2, mx: .5 }}/>
+                <Divider sx={{ my: 2, mx: .5 }} />
 
-                { playlists?.map((e) => <MenuItemLink key={e.id} title={e?.title} small to={`/playlist/${e.id}`}/>) }
+                <Link to="/playlist">
+                    <Box sx={{ width: "100%", display: "flex", p: .3 }}>
+                        <Typography variant="caption" color="primary" sx={{ cursor: "pointer", ":hover": { transform: "scale(1.1)" } }}>Playlists</Typography>
+                    </Box>
+                </Link>
 
+                {playlists?.map((e) => <MenuItemLink key={e.id} title={e?.title} small to={`/playlist/${e.id}`} />)}
             </MenuList>
         </Paper>
 
