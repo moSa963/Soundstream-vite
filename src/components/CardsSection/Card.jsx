d67e07cc4b29a3ecac3d995da1848f4ae49901cb
M S <mo.sa963@hotmail.com>
2023-05-05 10:36:16 +0300

    Update style

--- src/components/CardsSection/Card.jsx
+++ src/components/CardsSection/Card.jsx
@@ -1,4 +1,4 @@
-import { Box, Paper, Typography, styled } from "@mui/material";
+import { Box, Paper, styled } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -16,17 +16,17 @@ const Card = ({ src, onClick, children, to }) => {
             <Paper elevation={3} onClick={onClick}
                 sx={{
                     width: "100%",
-                    maxWidth: 230,
-                    minWidth: 150,
-                    height: 350,
-                    p: 1,
+                    maxWidth: { xs: 130, sm: 160, md: 200 },
+                    minWidth: { xs: 130, sm: 160, md: 200 },
+                    height: { xs: 230, md: 350 },
+                    p: { xs: .5, md: 1 },
                     overflow: "hidden",
                     cursor: "pointer",
                     m: .5,
                     boxShadow: t => t.shadows[2],
                     ":hover": { boxShadow: t => t.shadows[6], filter: "contrast(90%)" }
                 }}>
-                <Box sx={{ width: "100%", aspectRatio: '1', my: 1, boxShadow: t => t.shadows[1] }}>
+                <Box sx={{ width: "100%", aspectRatio: '1', my: { xs: .5, md: 1 }, boxShadow: t => t.shadows[1] }}>
                     <Image src={src} />
                 </Box>
                 {children}
