04ec0775d8ebeffd751eddcae8188adf08579937
M S <mo.sa963@hotmail.com>
2023-05-05 18:35:49 +0300

    Update spacing

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -7,7 +7,7 @@ import { APP_URL } from "../../utils/Request";
 const Track = ({ small, children, track }) => {
     
     return (
-        <Stack spacing={2} direction="row"  sx={{ width: "100%", height: "100%", minWidth: 100, p: small ? 0 : 1, }} alignItems="center">
+        <Stack spacing={small ? .5 : 2} direction="row"  sx={{ width: "100%", height: "100%", minWidth: 100, p: small ? 0 : 1, }} alignItems="center">
             <Avatar variant="square" sx={{ width: small ? 35 : 65, height: small ? 35 : 65 }} src={`${APP_URL}api/tracks/${track.id}/photo`}/>
             <Stack sx={{ overflow: "hidden", px: 1, "&>*:hover": { transform: "scale(1.05)" } }} title={track.title}>
                 <Link><Typography noWrap>{track.title}</Typography></Link>
