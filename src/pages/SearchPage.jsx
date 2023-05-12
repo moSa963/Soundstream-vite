108e103846e64cdc362d17bd1166b11c4938196b
M S <mo.sa963@hotmail.com>
2023-05-12 23:40:20 +0300

    Add loadData function and CardsSections

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -2,22 +2,42 @@ import { Box, Divider } from "@mui/material";
 import React from "react";
 import SearchBar from "../components/SearchBar";
 import CardsSection from "../components/CardsSection/CardsSection";
-
+import request from "../utils/Request";
+import PlaylistCard from "../components/CardsSection/PlaylistCard";
+import TrackCard from "../components/CardsSection/TrackCard";
+import UserCard from "../components/CardsSection/UserCard";
 
 
 const SearchPage = () => {
-    
+    const [data, setData] = React.useState({});
+
+    const handleLoad = (key) => {
+        loadData(key, setData);
+    }
 
     return (
         <Box sx={{ width: "100%", p: 2 }}>
-            <SearchBar />
+            <SearchBar onSearch={handleLoad}/>
 
             <Divider sx={{ my: 5 }} />
-
-            <CardsSection title="Recent searches"/>
+        
+            {Boolean(data?.tracks?.length)  && <CardsSection title="Tracks" Card={TrackCard} data={data?.tracks} noWrap/>}
+            {Boolean(data?.albums?.length) && <CardsSection title="Albums" Card={PlaylistCard} data={data?.albums} noWrap/>}
+            {Boolean(data?.playlists?.length) && <CardsSection title="Playlists" Card={PlaylistCard} data={data?.playlists} noWrap/>}
+            {Boolean(data?.users?.length) && <CardsSection title="Users" Card={UserCard} data={data?.users} noWrap/>}
         </Box>
     );
 }
 
+
+const loadData = async (key, setData) => {
+    const res = await request(`api/search/${key}`)
+
+    if (res.ok)
+    {
+        const js = await res.json();
+        setData(js.data);
+    }
+}
 
 export default SearchPage;
