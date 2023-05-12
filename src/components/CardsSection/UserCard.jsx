fd51c7f639aed13eb8684351e5d83834c7a04895
M S <mo.sa963@hotmail.com>
2023-05-12 23:38:55 +0300

    Add UserCard for CardsSection

---
+++ src/components/CardsSection/UserCard.jsx
@@ -0,0 +1,18 @@
+import { Typography } from "@mui/material";
+import React from "react";
+import Card from "./Card";
+import { APP_URL } from "../../utils/Request";
+
+
+const UserCard = ({ data }) => {
+
+    return (
+        <Card src={`${APP_URL}api/account/${data.username}/profile/photo`} to={`/user/${data.username}`}>
+            <Typography noWrap >{data?.name}</Typography>
+            <Typography variant="caption" color="gray" fontFamily="serif">@{data?.username}</Typography>
+        </Card>
+    );
+}
+
+
+export default UserCard;
