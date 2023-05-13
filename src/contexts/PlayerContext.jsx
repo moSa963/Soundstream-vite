5cd2ddf203266d5572652ad5b495c96ff2a20316
M S <mo.sa963@hotmail.com>
2023-05-13 23:57:35 +0300

    Add playTrack function that starts a track if it does exist in the list

--- src/contexts/PlayerContext.jsx
+++ src/contexts/PlayerContext.jsx
@@ -6,9 +6,17 @@ const Context = React.createContext();
 const PlayerProvider = ({ children }) => {
     const [list, setList] = React.useState([]);
     const [indices, setIndices] = React.useState([0]);
+
     const addTrack = React.useCallback((track) => {
-        setList(list => list.find(v => v.id == track.id) ? list : [...list, track]);
+        setList(list => (
+            list.find(v => v.id == track.id) ? list : [...list, track]
+        ));
     }, []);
+
+    const playTrack = React.useCallback((track) => {
+        const index = list.findIndex(v => v.id == track.id);
+        index != -1 && setIndices(i => [...i, index]);
+    }, [list]);
 
     const handleForward = (shuffle) => {
         setIndices(i => [...i, (shuffle ? (Math.floor(Math.random() * 10)) : indices[indices.length - 1] + 1) % list.length]);
@@ -19,9 +27,12 @@ const PlayerProvider = ({ children }) => {
     }
 
     return (
-        <Context.Provider value={{ indices, setIndices, list, setList, addTrack }}>
+        <Context.Provider value={{ indices, setIndices, list, setList, addTrack, playTrack }}>
             {children}
-            {list.length > 0 && <AudioPlayer track={list[indices[indices.length - 1]]} onForward={handleForward} onBackward={handleBackward}/>}
+            {
+                list.length > 0 &&
+                <AudioPlayer track={list[indices[indices.length - 1]]} onForward={handleForward} onBackward={handleBackward} />
+            }
         </Context.Provider>
     );
 }
