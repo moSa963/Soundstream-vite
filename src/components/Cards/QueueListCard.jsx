357da2e8000093f1f8305e1a5c1260b3fa16d03a
M S <mo.sa963@hotmail.com>
2023-05-14 22:41:08 +0300

    Add clear button that empty the queue

--- src/components/Cards/QueueListCard.jsx
+++ src/components/Cards/QueueListCard.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { usePlayer } from "../../contexts/PlayerContext"
 import CardBase from "./CardBase";
-import { Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import TracksTable from "../TracksTable/TracksTable";
+import { ClearAll } from "@mui/icons-material";
 
 
 const QueueListCard = ({ open, setOpen }) => {
     const { list, setList, setIndices } = usePlayer();
 
+
+
     return (
         <CardBase open={open} setOpen={setOpen} maxHeight={500} >
             <Typography sx={{ width: "100%" }} variant="h4">Queue</Typography>
+
+            <IconButton title="clear" sx={{ position: "absolute", top: 10, right: 10 }} color="error" onClick={() => setList([])}>
+                <ClearAll />
+            </IconButton>
 
             <TracksTable 
                 tracks={list}
