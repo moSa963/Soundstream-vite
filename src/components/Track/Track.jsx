748a2a1c98c00c56aadf7510d39e16f57ebaabfb
M S <mo.sa963@hotmail.com>
2023-05-14 23:12:23 +0300

    Add showTrack route link

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -5,13 +5,17 @@ import { APP_URL } from "../../utils/Request";
 
 
 const Track = ({ small, children, track }) => {
-    
+
     return (
-        <Stack spacing={small ? .5 : 2} direction="row"  sx={{ width: "100%", height: "100%", minWidth: 100, p: small ? 0 : 1, }} alignItems="center">
-            <Avatar variant="square" sx={{ width: small ? 35 : 65, height: small ? 35 : 65 }} src={`${APP_URL}api/tracks/${track.id}/photo`}/>
+        <Stack spacing={small ? .5 : 2} direction="row" sx={{ width: "100%", height: "100%", minWidth: 100, p: small ? 0 : 1, }} alignItems="center">
+            <Avatar variant="square" sx={{ width: small ? 35 : 65, height: small ? 35 : 65 }} src={`${APP_URL}api/tracks/${track.id}/photo`} />
             <Stack sx={{ overflow: "hidden", px: 1, "&>*:hover": { transform: "scale(1.05)" } }} title={track.title}>
-                <Link><Typography noWrap sx={{ fontSize: { xs: 14, sm: 16 } }}>{track.title}</Typography></Link>
-                <Link to={`/user/${track.user.username}`}><Typography variant="caption" color="GrayText">{track.user.username}</Typography></Link>
+                <Link to={`/track/${track.id}`}>
+                    <Typography noWrap sx={{ fontSize: { xs: 14, sm: 16 } }}>{track.title}</Typography>
+                </Link>
+                <Link to={`/user/${track.user.username}`}>
+                    <Typography variant="caption" color="GrayText">{track.user.username}</Typography>
+                </Link>
             </Stack>
             {children}
         </Stack>
