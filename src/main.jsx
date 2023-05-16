ec6ce1f118ac68a71fffee18978b37303315af4a
M S <mo.sa963@hotmail.com>
2023-05-16 23:19:45 +0300

    Move BrowserRouter from components to router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
 import ThemeProvider from "./contexts/ThemeContext";
 import { RouterProvider } from 'react-router-dom';
-import {createRoutes} from "./components/BrowserRouter";
+import {createRoutes} from "./router/BrowserRouter";
 
 const router = createRoutes();
 
