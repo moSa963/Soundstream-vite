f6edbc3f17dea09d63c586b853531a51933189e7
M S <mo.sa963@hotmail.com>
2023-06-04 15:53:32 +0300

    Add 'PaginationList' component
    component that load a list of data and pass 'loadNext' method which load the next set of data

---
+++ src/components/PaginationList.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useMessage } from "../contexts/MessageContext";
+import request from "../utils/Request";
+
+
+
+const PaginationList = ({ url, loader }) => {
+    const [list, setList] = React.useState([]);
+    const [next, setNext] = React.useState(null);
+    const [progress, setProgress] = React.useState(false);
+    const { setMessage } = useMessage();
+
+    React.useEffect(() => {
+        !progress && loadData(url, setList, setNext, setProgress, setMessage);
+    }, [url, setMessage]);
+
+    return loader(list, setList, () => loadData(next, setList, setNext, setProgress, setMessage, true));
+}
+
+const loadData = async (url, setList, setNext, setProgress, setMessage, append) => {
+    if (url == null)
+    {
+        return;
+    }
+
+    setProgress(true);
+
+    try {
+        const res = await request(url);
+        const js = await res.json();
+        setNext(js.links.next);
+        setList(data => append ? [...data, ...js.data] : js.data);
+    } catch (error) {
+        setMessage({ type: "error", title: error });
+    }
+
+    setProgress(false);
+}
+
+export default PaginationList;
