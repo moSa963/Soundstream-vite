24806f787903e070f240e26b8875578722d9985b
M S <mo.sa963@hotmail.com>
2023-06-07 22:59:03 +0300

    Remove CreateAlbumCard from LibraryPage

--- src/pages/LibraryPage.jsx
+++ src/pages/LibraryPage.jsx
@@ -1,6 +1,5 @@
 import { Box } from "@mui/material";
 import React from "react";
-import CreateAlbumCard from "../components/Dialogs/CreateAlbumCard";
 
 
 const LibraryPage = () => {
@@ -8,7 +7,6 @@ const LibraryPage = () => {
 
     return (
         <Box sx={{ width: "100%", pt: 3 }}>
-            <CreateAlbumCard onAlbumAdded={(v) => setAlbums(as => [...as, v])}/>
         </Box>
     );
 }
