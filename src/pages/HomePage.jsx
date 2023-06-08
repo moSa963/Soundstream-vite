99c85e4228df205e3bdfb95efc88a2b3b76a09f2
M S <mo.sa963@hotmail.com>
2023-06-08 22:39:14 +0300

    Add data to home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,19 +2,61 @@ import { Box } from "@mui/material";
 import React from "react";
 import CardsSection from "../components/CardsSection/CardsSection";
 import { useNavigate } from "react-router-dom";
+import { usePlaylists } from "../contexts/PlaylistsContext";
+import PlaylistCard from "../components/CardsSection/PlaylistCard";
+import PaginationList from "../components/PaginationList";
+import TrackCard from "../components/CardsSection/TrackCard";
 
 
 
 const HomePage = () => {
     const nav = useNavigate();
+    const { playlists } = usePlaylists();
 
     return (
         <Box sx={{ width: "100%" }}>
-            
-            <CardsSection title="Your Playlists" onAction={() => nav("/playlist")} actionTitle="ShowAll" />
-            <CardsSection title="Recently Played"/>
-            <CardsSection title="Liked Tracks"/>
-            <CardsSection title="Recent searches"/>
+
+            <CardsSection
+                title="Your Playlists"
+                onAction={() => nav("/library/playlists")}
+                actionTitle="ShowAll"
+                Card={PlaylistCard}
+                data={playlists.filter((v, i) => !v.album).filter((_, i) => i < 4)}
+            />
+
+            <CardsSection
+                title="Albums"
+                onAction={() => nav("/library/albums")}
+                actionTitle="ShowAll"
+                Card={PlaylistCard}
+                data={playlists.filter(v => v.album).filter((_, i) => i < 4)}
+            />
+
+            <PaginationList
+                url="api/likes?count=4"
+                loader={(data) => (
+                    <CardsSection
+                        title="Liked Tracks"
+                        onAction={() => nav("/likes")}
+                        actionTitle="ShowAll"
+                        Card={TrackCard}
+                        data={data}
+                    />
+                )}
+            />
+
+            <PaginationList
+                url="api/history/tracks?count=4"
+                loader={(data) => (
+                    <CardsSection
+                        title="Recently Played"
+                        onAction={() => nav("/likes")}
+                        actionTitle="ShowAll"
+                        Card={TrackCard}
+                        data={data}
+                    />
+                )}
+            />
 
         </Box>
     );
