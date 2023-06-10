f83585ba389db1831f8d2269e05f0be0c6fe2375
M S <mo.sa963@hotmail.com>
2023-06-10 21:41:07 +0300

    Refactor PaginationList to lazy load data
    pagination list now load data only if the top of the main container is less than 800px which means it about to be visible on the screen otherwise it will show Skeleton

--- src/components/PaginationList.jsx
+++ src/components/PaginationList.jsx
@@ -1,24 +1,47 @@
 import React from "react";
 import { useMessage } from "../contexts/MessageContext";
 import request from "../utils/Request";
+import { Box } from "@mui/material";
+import { useScroll } from "../contexts/ScrollContext";
 
 
-const PaginationList = ({ url, loader }) => {
-    const [list, setList] = React.useState([]);
+const PaginationList = ({ url, loader, Skeleton }) => {
+    const ref = React.useRef();
+    const [list, setList] = React.useState(null);
     const [next, setNext] = React.useState(null);
+    const [visible, setVisible] = React.useState(false);
     const [progress, setProgress] = React.useState(false);
+    const { scroll } = useScroll();
     const { setError } = useMessage();
 
-    React.useEffect(() => {
-        !progress && loadData(url, setList, setNext, setProgress, setError);
-    }, [url, setError]);
+    const loadNext = React.useCallback(() => !progress && loadData(next, setList, setNext, setError, setProgress, true), [next, progress]);
 
-    return loader(list, next, setList, () => loadData(next, setList, setNext, setProgress, setError, true));
+    const loadedData = React.useMemo(() => loader(list, next, setList, loadNext), [loader, list, next, loadNext]);
+
+    React.useEffect(() => {
+        setList(null);
+    }, [url]);
+
+    React.useEffect(() => {
+        setVisible(ref.current.getBoundingClientRect().top < 800);
+    }, [ref.current, scroll]);
+
+    React.useEffect(() => {
+        if (list === null && visible && !progress) {
+            loadData(url, setList, setNext, setError, setProgress);
+        }
+    }, [url, setError, list, visible, progress]);
+
+    return (
+        <Box ref={ref}>
+            {list !== null && loadedData}
+            {list === null && Skeleton && <Skeleton />}
+        </Box>
+    )
 }
 
-const loadData = async (url, setList, setNext, setProgress, setError, append) => {
-    if (url == null)
-    {
+const loadData = async (url, setList, setNext, setError, setProgress, append) => {
+    if (url == null) {
         return;
     }
 
