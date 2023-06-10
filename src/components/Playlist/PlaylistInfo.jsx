337efdce955c0e06cc3341267525fab0b5c48e43
M S <mo.sa963@hotmail.com>
2023-06-10 10:39:10 +0300

    Add display condition to trackCount in playlist Info

--- src/components/Playlist/PlaylistInfo.jsx
+++ src/components/Playlist/PlaylistInfo.jsx
@@ -15,11 +15,11 @@ const PlaylistInfo = ({ user, trackCount }) => {
                     <Stack direction="row" spacing={.5} sx={{ cursor: "pointer", color: t => t.palette.text.secondary, ":hover": { color: t => t.palette.text.primary } }}>
                         <Avatar sx={{ width: 25, height: 25 }} src={`${APP_URL}api/account/${user.username}/profile/photo`} />
                         <Typography noWrap>{user.username}</Typography>
+                        <Typography>&bull;</Typography>
                     </Stack>
                 </Link>
             }
-            <Typography>&bull;</Typography>
-            <Typography>{trackCount} {trackCount > 1 ? "tracks" : "track"}</Typography>
+            {trackCount != null && <Typography>{trackCount} {trackCount > 1 ? "tracks" : "track"}</Typography>}
         </Stack>
     );
 }
