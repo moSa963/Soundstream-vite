9c3f26781daaa03fe6cd04f36dac2d8c4a33fc2d
M S <mo.sa963@hotmail.com>
2023-06-10 10:41:49 +0300

    Remove unused imports

--- src/components/TracksTable/TracksTable.jsx
+++ src/components/TracksTable/TracksTable.jsx
@@ -2,48 +2,45 @@ import React from "react";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import TracksTableRow from "./TracksTableRow";
 import { Timelapse } from "@mui/icons-material";
 
 
 const TracksTable = ({ tracks, setTracks, actions, onAction, simple, onPlay, stickyHeader }) => {
-
     const handleSetLiked = (track, liked) => {
         track.liked = liked;
         setTracks(t => [...t]);
     }
 
     return (
-            <Table sx={{ width: "100%", "& td": { padding: .5 } }} size="small" stickyHeader={stickyHeader}>
-                <TableHead sx={{ position: stickyHeader ? "sticky" : "static", top: 45, zIndex: 2 }}>
-                    <TableRow  > 
-                        <TableCell >#</TableCell>
-                        <TableCell width="100%">Title</TableCell>
-                        {!simple && <TableCell align="center">Album</TableCell>}
-                        {!simple && <TableCell align="center">Date added</TableCell>}
-                        {!simple && <TableCell align="center"></TableCell>}
-                        {!simple && <TableCell align="center"><Timelapse /></TableCell>}
-                        <TableCell></TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {tracks?.map((track, index) =>
-                        <TracksTableRow key={track?.id} 
-                            simple={simple}
-                            track={track}
-                            onPlay={onPlay}
-                            index={index}
-                            setLiked={(v) => handleSetLiked(track, v)}
-                            actions={actions}
-                            onAction={onAction}
-                        />
-                    )}
-                </TableBody>
-            </Table>
+        <Table sx={{ width: "100%", "& td": { padding: .5 } }} size="small" stickyHeader={stickyHeader}>
+            <TableHead sx={{ position: stickyHeader ? "sticky" : "static", top: 45, zIndex: 2 }}>
+                <TableRow  > 
+                    <TableCell >#</TableCell>
+                    <TableCell width="100%">Title</TableCell>
+                    {!simple && <TableCell align="center">Album</TableCell>}
+                    {!simple && <TableCell align="center">Date added</TableCell>}
+                    {!simple && <TableCell align="center"></TableCell>}
+                    {!simple && <TableCell align="center"><Timelapse /></TableCell>}
+                    <TableCell></TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {tracks?.map((track, index) =>
+                    <TracksTableRow key={track?.id} 
+                        simple={simple}
+                        track={track}
+                        onPlay={onPlay}
+                        index={index}
+                        setLiked={(v) => handleSetLiked(track, v)}
+                        actions={actions}
+                        onAction={onAction}
+                    />
+                )}
+            </TableBody>
+        </Table>
     );
 }
 
