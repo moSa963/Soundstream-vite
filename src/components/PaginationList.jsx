bb54b4b410f80f329f417ee7ff5d47ec5f901f5d
M S <mo.sa963@hotmail.com>
2023-06-11 09:15:51 +0300

    Refactor PaginationList to remove unnecessary state 'visible'

--- src/components/PaginationList.jsx
+++ src/components/PaginationList.jsx
@@ -9,28 +9,23 @@ const PaginationList = ({ url, loader, Skeleton }) => {
     const ref = React.useRef();
     const [list, setList] = React.useState(null);
     const [next, setNext] = React.useState(null);
-    const [visible, setVisible] = React.useState(false);
     const [progress, setProgress] = React.useState(false);
     const { scroll } = useScroll();
     const { setError } = useMessage();
 
     const loadNext = React.useCallback(() => !progress && loadData(next, setList, setNext, setError, setProgress, true), [next, progress]);
 
-    const loadedData = React.useMemo(() => loader(list, next, setList, loadNext), [loader, list, next, loadNext]);
+    const loadedData = React.useMemo(() => loader(list, next, setList, loadNext), [list, next, loadNext]);
 
     React.useEffect(() => {
         setList(null);
     }, [url]);
 
     React.useEffect(() => {
-        setVisible(ref.current.getBoundingClientRect().top < 800);
-    }, [ref.current, scroll]);
-
-    React.useEffect(() => {
-        if (list === null && visible && !progress) {
+        if (list === null && !progress && ref.current.getBoundingClientRect().top < 800) {
             loadData(url, setList, setNext, setError, setProgress);
         }
-    }, [url, setError, list, visible, progress]);
+    }, [url, setError, list, progress, scroll]);
 
     return (
         <Box ref={ref}>
