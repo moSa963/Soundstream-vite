4753934bcd9d6ff52783587a94b0e99321620ee1
M S <mo.sa963@hotmail.com>
2023-06-11 18:27:27 +0300

    Add play button to TrackCard

--- src/components/CardsSection/TrackCard.jsx
+++ src/components/CardsSection/TrackCard.jsx
@@ -1,22 +1,46 @@
-import { Typography } from "@mui/material";
+import { Box, IconButton, Paper, Typography } from "@mui/material";
 import React from "react";
 import Card from "./Card";
 import { APP_URL } from "../../utils/Request";
 import { usePlayer } from "../../contexts/PlayerContext";
-
+import PlayCircleIcon from '@mui/icons-material/PlayCircleFilled';
+import { useNavigate } from "react-router-dom";
 
 const TrackCard = ({ data }) => {
     const { addTrack, playTrack } = usePlayer();
+    const nav = useNavigate();
 
-    const handlePlay = () => {
+    const handlePlay = (e) => {
+        e.stopPropagation();
         addTrack(data);
         playTrack(data);
     }
 
     return (
-        <Card src={`${APP_URL}api/tracks/${data.id}/photo`} onClick={handlePlay}>
+        <Card src={`${APP_URL}api/tracks/${data.id}/photo`} onClick={() => nav(`/track/${data.id}`)}>
             <Typography noWrap >{data?.title}</Typography>
             <Typography variant="caption" color="gray" fontFamily="serif">@{data?.user.username}</Typography>
+
+            <Box sx={{
+                position: "absolute",
+                inset: "0 0 0 0",
+                display: "flex",
+                justifyContent: "end",
+                alignItems: "end",
+                opacity: 0,
+                p: 1,
+                transition: "500ms",
+                background: t => `linear-gradient(174deg, rgba(0,0,0,0) 10%, ${t.palette.background.paper} 100%)`,
+                ":hover": {
+                    opacity: 1,
+                }
+            }}>
+                <IconButton onClick={handlePlay} size="small">
+                    <Paper sx={{ backgroundColor: t=>t.palette.background.paper, borderRadius: 99999999999, display: "flex", justifyContent: "center", alignItems: "center" }}>
+                        <PlayCircleIcon sx={{ width: 75, height: 75 }}/>
+                    </Paper>
+                </IconButton>
+            </Box>
         </Card>
     );
 }
