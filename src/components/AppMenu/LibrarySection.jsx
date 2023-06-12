4a34a7ae255a1bcb0075bd46167877d18261ab66
M S <mo.sa963@hotmail.com>
2023-06-12 22:15:37 +0300

    Add LibrarySection

---
+++ src/components/AppMenu/LibrarySection.jsx
@@ -0,0 +1,27 @@
+import { Box } from "@mui/material";
+import React from "react";
+import { usePlaylists } from "../../contexts/PlaylistsContext";
+import LibraryAdd from "@mui/icons-material/LibraryAdd";
+import MenuItemLink from "./MenuItemLink";
+import CreatePlaylistCard from "../Dialogs/CreatePlaylistCard";
+import PlaylistItem from "../Playlist/PlaylistItem";
+
+const LibrarySection = () => {
+    const { playlists } = usePlaylists();
+
+    return (
+        <Box sx={{ display: "block", overflow: "hidden", width: "100%", height: "100%" }}>
+            <Box alignItems="center" sx={{ display: "flex", flexDirection: { xs: "column", sm: "row" }, alignItems: { xs: "start", sm: "center" } }}>
+                <MenuItemLink Icon={LibraryAdd} title="Library" to="library" />
+                <Box sx={{ flexGrow: 1 }} />
+                <CreatePlaylistCard />
+            </Box>
+
+            <Box sx={{ display: "block", overflow: "auto", width: "100%", height: "100%" }} >
+                {playlists?.map((v) => <PlaylistItem key={v.id} playlist={v} small />)}
+            </Box>
+        </Box>
+    );
+}
+
+export default LibrarySection;
