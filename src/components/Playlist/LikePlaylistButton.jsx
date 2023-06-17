3b623601e154408b88e683c082478c94e1a60ae1
M S <mo.sa963@hotmail.com>
2023-06-17 10:58:29 +0300

    Fix 'setError' is undefined

--- src/components/Playlist/LikePlaylistButton.jsx
+++ src/components/Playlist/LikePlaylistButton.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import request from "../../utils/Request";
 import LikeButton from "../LikeButton";
+import { useMessage } from "../../contexts/MessageContext";
 
 
 const LikePlaylistButton = ({ playlist, setLiked, large }) => {
-
+    const {setError} = useMessage();
 
     return (
-        <LikeButton liked={playlist?.liked} setLiked={() => like(playlist, setLiked)} large={large}/>
+        <LikeButton liked={playlist?.liked} setLiked={() => like(playlist, setLiked, setError)} large={large}/>
     );
 }
 
