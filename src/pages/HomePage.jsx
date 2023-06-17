c47c3a64ca1f1c35942be9766ca7343a641cdbe3
M S <mo.sa963@hotmail.com>
2023-06-17 20:00:10 +0300

    Add Liked Tracks to home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,9 @@ import CardsSection from "../components/CardsSection/CardsSection";
 import { useNavigate } from "react-router-dom";
 import { usePlaylists } from "../contexts/PlaylistsContext";
 import PlaylistCard from "../components/CardsSection/PlaylistCard";
+import PaginationList from "../components/PaginationList";
+import CardsSectionSkeleton from "../components/CardsSection/CardsSectionSkeleton";
+import TrackCard from "../components/CardsSection/TrackCard";
 
 
 
@@ -19,7 +22,21 @@ const HomePage = () => {
                 onAction={() => nav("/library/playlists")}
                 actionTitle="ShowAll"
                 Card={PlaylistCard}
-                data={playlists.filter((v, i) => !v.album).filter((_, i) => i < 4)}
+                data={playlists.filter((v, i) => !v.album).filter((_, i) => i < 6)}
+            />
+
+            <PaginationList
+                url="api/likes?count=6"
+                Skeleton={CardsSectionSkeleton}
+                loader={(data) => (
+                    <CardsSection
+                        title="Liked Tracks"
+                        onAction={() => nav("/likes")}
+                        actionTitle="ShowAll"
+                        Card={TrackCard}
+                        data={data}
+                    />
+                )}
             />
 
             <CardsSection
@@ -29,7 +46,6 @@ const HomePage = () => {
                 Card={PlaylistCard}
                 data={playlists.filter(v => v.album).filter((_, i) => i < 4)}
             />
-
         </Box>
     );
 }
