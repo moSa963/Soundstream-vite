ebecd6b0d2bbda52e8fa7cf2642702cc92c26019
M S <mo.sa963@hotmail.com>
2023-06-17 20:00:37 +0300

    Add Buttons list

--- src/pages/LibraryPage.jsx
+++ src/pages/LibraryPage.jsx
@@ -1,51 +1,37 @@
-import { Box } from "@mui/material";
+import { Box, Button, ButtonGroup, IconButton, Typography } from "@mui/material";
 import React from "react";
 import CardsSection from "../components/CardsSection/CardsSection";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { usePlaylists } from "../contexts/PlaylistsContext";
-import PlaylistCard from "../components/CardsSection/PlaylistCard";
 import PaginationList from "../components/PaginationList";
 import TrackCard from "../components/CardsSection/TrackCard";
 import CardsSectionSkeleton from "../components/CardsSection/CardsSectionSkeleton";
+import HorizontalList from "../components/HorizontalList";
+import { Add } from "@mui/icons-material";
+import CreateAlbumCard from "../components/Dialogs/CreateAlbumCard";
 
 
 
 const LibraryPage = () => {
     const nav = useNavigate();
-    const { playlists } = usePlaylists();
+    const { setPlaylists } = usePlaylists();
+
+    const handleAlbumAdded = (p) => {
+        setPlaylists(ps => [...ps, p]);
+        nav(`/library/albums/${p.id}`);
+    }
 
     return (
         <Box sx={{ width: "100%" }}>
-
-            <CardsSection
-                title="Your Playlists"
-                onAction={() => nav("/library/playlists")}
-                actionTitle="ShowAll"
-                Card={PlaylistCard}
-                data={playlists.filter((v, i) => !v.album).filter((_, i) => i < 4)}
-            />
-
-            <CardsSection
-                title="Albums"
-                onAction={() => nav("/library/albums")}
-                actionTitle="ShowAll"
-                Card={PlaylistCard}
-                data={playlists.filter(v => v.album).filter((_, i) => i < 4)}
-            />
-
-            <PaginationList
-                url="api/likes?count=4"
-                Skeleton={CardsSectionSkeleton}
-                loader={(data) => (
-                    <CardsSection
-                        title="Liked Tracks"
-                        onAction={() => nav("/likes")}
-                        actionTitle="ShowAll"
-                        Card={TrackCard}
-                        data={data}
-                    />
-                )}
-            />
+            <HorizontalList  >
+                <CreateAlbumCard onAlbumAdded={handleAlbumAdded}/>        
+                <Button fullWidth onClick={() => nav("/library/playlists")}>
+                    <Typography>Playlists</Typography>
+                </Button>
+                <Button fullWidth onClick={() => nav("/library/albums")}>
+                    <Typography>Albums</Typography>
+                </Button>
+            </HorizontalList>
 
             <PaginationList
                 url="api/history/tracks"
