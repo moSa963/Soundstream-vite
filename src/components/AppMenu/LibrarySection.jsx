8d641ca24e581e0b5638ea91520bb13ccc0657a1
M S <mo.sa963@hotmail.com>
2023-06-17 17:48:19 +0300

    Add 'AnimatedList' to display playlists in the app menu

--- src/components/AppMenu/LibrarySection.jsx
+++ src/components/AppMenu/LibrarySection.jsx
@@ -5,21 +5,23 @@ import LibraryAdd from "@mui/icons-material/LibraryAdd";
 import MenuItemLink from "./MenuItemLink";
 import CreatePlaylistCard from "../Dialogs/CreatePlaylistCard";
 import PlaylistItem from "../Playlist/PlaylistItem";
+import AnimatedList from "../AnimatedList/AnimatedList";
 
 const LibrarySection = () => {
     const { playlists } = usePlaylists();
 
     return (
-        <Box sx={{ display: "flex", flexDirection: "column", flex: 1, overflow: "hidden", width: "100%" }}>
+        <Box sx={{ display: "flex", flexDirection: "column", flex: 1, overflow: "hidden", width: "100%", bgcolor: "inherit" }}>
             <Box alignItems="center" sx={{ display: "flex", flexDirection: { xs: "column", sm: "row" }, alignItems: { xs: "start", sm: "center" } }}>
                 <MenuItemLink Icon={LibraryAdd} title="Library" to="library" />
                 <Box sx={{ flexGrow: 1 }} />
                 <CreatePlaylistCard />
             </Box>
 
-            <Box sx={{ display: "block", overflow: "auto", width: "100%", height: "100%" }} >
-                {playlists?.map((v) => <PlaylistItem key={v.id} playlist={v} small />)}
-            </Box>
+            <AnimatedList 
+                values={playlists}
+                map={(v) => <PlaylistItem key={v.id} playlist={v} small />}
+            />
         </Box>
     );
 }
