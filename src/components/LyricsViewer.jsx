7199dccf4d98339e2a797659a06a6fa6480c04e0
M S <mo.sa963@hotmail.com>
2023-09-26 22:23:48 +0300

    Fix stamps are not synchronized correctly

--- src/components/LyricsViewer.jsx
+++ src/components/LyricsViewer.jsx
@@ -5,6 +5,7 @@ import React from "react";
 const LyricsViewer = ({ lyrics = "", stamps = [], current = 0, onClick, flex }) => {
     const ref = React.useRef(null);
     const [index, setIndex] = React.useState(0);
+    const currentRef = React.useRef(null);
 
     React.useEffect(() => {
         setIndex(v => {
@@ -18,41 +19,47 @@ const LyricsViewer = ({ lyrics = "", stamps = [], current = 0, onClick, flex }) 
     }, [current, stamps]);
 
     React.useEffect(() => {
-        if (ref.current == null || index == -1) {
-            return;
-        }
-
-        const nodes = ref.current.childNodes;
-        var i = nodes.length > index ? index : nodes.length - 1;
-
-        nodes[i].scrollIntoView({ behavior: "smooth", block: "center", inline: "start" });
-    }, [ref.current, index]);
+        currentRef.current?.scrollIntoView({ behavior: "smooth", block: "center", inline: "start" });
+    }, [currentRef.current, index]);
 
     return (
         <Box sx={{ width: "100%", height: "100%", overflow: "auto", overflowX: "hidden", flex: flex }} ref={ref} >
             {
-                lyrics.split(/[\r\n]/).map(
-                    (line, i) =>
-                        <React.Fragment key={i}>
-                            {
-                                line.length > 0
-                                    ? <Typography
-                                        onClick={() => onClick && onClick(line, i)}
-                                        variant="h5"
-                                        sx={{ width: "fit-content", opacity: index == i ? 1 : 0.8, cursor: "pointer", wordBreak: "break-word", transformOrigin: "left", ":hover": { transform: "scale(1.05)" } }}
-                                        color={index == i ? "primary" : null}
-                                        fontFamily="cursive"
-                                        fontWeight="bold" >
-                                        {line}
-                                    </Typography>
-                                    : <br />
-                            }
-                        </React.Fragment>
-                )
+                mapLyrics(lyrics, index, onClick, currentRef)
             }
         </Box>
     );
 }
 
+
+const mapLyrics = (lyrics, index, onClick, currentRef) => {
+    var whitespace = 0;
+
+    return lyrics
+        .split(/[\r\n]/)
+        .map(
+            (line, i) => {
+                if (line.length <= 0) {
+                    ++whitespace;
+                    return <br key={i} />;
+                }
+
+                var key = i - whitespace;
+                const props = key == index ? { ref: currentRef } : {};
+                return (
+                    <Typography key={i}
+                        onClick={() => onClick && onClick(line, key)}
+                        variant="h5"
+                        sx={{ width: "fit-content", opacity: index == key ? 1 : 0.8, cursor: "pointer", wordBreak: "break-word", transformOrigin: "left", ":hover": { transform: "scale(1.05)" } }}
+                        color={index == key ? "primary" : null}
+                        fontFamily="cursive"
+                        fontWeight="bold"
+                        {...props}>
+                        {line}
+                    </Typography>
+                );
+            }
+        )
+}
 
 export default LyricsViewer;
