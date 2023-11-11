d7ece38911292cccffa4fc063c54756622769750
M S <mo.sa963@hotmail.com>
2023-11-11 15:20:44 +0300

    Fix img does not appear in PlaylistCard

--- src/components/CardsSection/PlaylistCard.jsx
+++ src/components/CardsSection/PlaylistCard.jsx
@@ -7,7 +7,7 @@ import { APP_URL } from "../../utils/Request";
 const PlaylistCard = ({ data }) => {
 
     return (
-        <Card src={`${APP_URL}api/playlists/${data.id}/photo`} to={`/library/playlists/${data.id}`}>
+        <Card src={`${APP_URL}api/playlists/${data.id}/photo/${data.image}`} to={`/library/playlists/${data.id}`}>
             <Typography noWrap >{data?.title}</Typography>
             <Typography variant="caption" color="gray" fontFamily="serif">{data?.description}</Typography>
         </Card>
