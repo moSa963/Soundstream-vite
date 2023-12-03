4ccaf9b391807b16abe52dd4ddda8e0b174a6158
M S <mo.sa963@hotmail.com>
2023-12-03 16:16:09 +0300

    Add 'onDominantColorLoad' callback to AvatarInput to pass the dominant color of the avatar

--- src/components/AvatarInput.jsx
+++ src/components/AvatarInput.jsx
@@ -1,15 +1,27 @@
 import { AddAPhoto } from "@mui/icons-material";
-import { Avatar, Box, Button, ButtonBase, IconButton } from "@mui/material";
+import { Avatar, Box, Button } from "@mui/material";
 import React from "react";
+import { dominantColor } from "../utils/colors";
 
 
 
-const AvatarInput = ({ src, sx, disabled, onChange }) => {
+const AvatarInput = ({ src, sx, disabled, onChange, onDominantColorLoad }) => {
     const [hover, setHover] = React.useState(false);
+
+    const handleLoad = (img) => {
+        const canv = document.createElement("canvas");
+        canv.width = img.width / 2;
+        canv.height = img.height / 2;
+        
+        const ctx = canv.getContext("2d");
+        ctx.drawImage(img, 0, 0, canv.width, canv.height);
+        const imageData = ctx.getImageData(0, 0, canv.width, canv.height);
+        onDominantColorLoad && onDominantColorLoad(dominantColor(imageData.data));
+    }
 
     return (
         <Box sx={{ position: "relative" }} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
-            <Avatar src={src} sx={{...sx,  opacity: hover && !disabled ? 0.1 : 1}} variant="square"  />
+            <Avatar src={src} sx={{ ...sx, opacity: hover && !disabled ? 0.1 : 1 }} variant="square" onLoad={(e) => handleLoad(e.target)} />
             {hover && !disabled &&
                 <Button sx={{ position: "absolute", inset: "0 0 0 0" }}
                     onClick={(e) => e.currentTarget.children[1].click()}
